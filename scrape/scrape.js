a0be00b80081363d636513c9b07abc28f141b440
Prasheel <prasheel.soni@hotwaxsystems.com>
2018-01-10 16:06:44 +0530

    Updated scraper

--- scrape/scrape.js
+++ scrape/scrape.js
@@ -35,49 +35,54 @@ router.get('/gps',function(req,res){
     var formattedStateName = names[1].replace(' ','_');
 
     mongodb.connect(dbUrl,function(mongoErr,db){
-
-      db.collection('cities').findOne({"stateName": formattedStateName}).then(function(data){
+      db.collection('cities').findOne({"name": new RegExp(names[0])}).then(function(data){
         console.log('Got City availability : '+data!==null);
         if(!data){
-          console.log('State not in DB, Searching and inserting cities...');
-          util.insertCitiesIntoDb(formattedStateName, db).then(function(data){
+
+          console.log('City %s not in DB, Searching and inserting city...', names[0]);
+          util.insertCitiesIntoDb(names, db).then(function(data){
             console.log('Data Inserted  : '+data);
-            db.collection('cities').findOne({name:new RegExp(names[0])}).then(function(rcd){
+            db.collection('cities').findOne({name:new RegExp(names[0])}).then(function(rcd, err){
               var response = {};
               if (rcd) {
-                request({
-                  url: "http://www.mypetrolprice.com/Default.aspx?LocationID=" + rcd.city_id, headers: {
-                    'User-Agent': 'request'
-                  }
-                }, function (errPrice, responsePrice, htmlPrice) {
-                  var $ = cheerio.load(htmlPrice);
-                  $('.hideInMobileDiv').filter(function () {
-                    var data = $(this);
-                    response['city'] = names[0];
-                    if(!data.attr('id')) {
-                      var text = data.text();
-                      text = text.replace('Rs','');
-                      response[data.siblings().first().text()] = "₹ " + text;
-                      text='';
-                    }
-                  });
-                  res.send(JSON.stringify(response));
-                });
+                // request({
+                //   url: "http://www.mypetrolprice.com/Default.aspx?LocationID=" + rcd.city_id, headers: {
+                //     'User-Agent': 'request'
+                //   }
+                // }, function (errPrice, responsePrice, htmlPrice) {
+                //   var $ = cheerio.load(htmlPrice);
+                //   $('.hideInMobileDiv').filter(function () {
+                //     var data = $(this);
+                //     response['city'] = names[0];
+                //     if(!data.attr('id')) {
+                //       var text = data.text();
+                //       text = text.replace('Rs','');
+                //       response[data.siblings().first().text()] = "₹ " + text;
+                //       text='';
+                //     }
+                //   });
+                //   res.send(JSON.stringify(response));
+                // });
+                util.getPricesInCity(rcd.city_id, rcd.name).then(function(data){
+                  res.send(data);
+                })
               }
               else {
                 db.collection('cities').findOne(
                   {name: {$regex :new RegExp(names[1])}}
-                ).then(function (stateRcd) {
+                ).then(function (err,stateRcd) {
                   if(stateRcd){
                     util.getPricesInCity(rcd.city_id, rcd.name).then(function (value) {
                       res.send(value);
-                    })
+                    }).catch(function (reason) { console.log('===========reason4=========',reason); })
                   }
                   else{
                     response['city'] = null;
                     res.send(JSON.stringify(response));
                   }
-                });
+                }).catch(function (error) {
+                  console.log('===========rerror=========',error);
+                })
               }
             }).catch(function(err){
               console.log(err);
@@ -85,77 +90,111 @@ router.get('/gps',function(req,res){
           }).catch(function(err){
             console.log(err)
           })
+
+
         }
         else{
           console.log(names[0]+", "+names[1]+", India");
           db.collection('cities').findOne(
             {name: {$regex :new RegExp(names[0])}}
-          ).then(function(rcd){
+          ).then(function(rcd, err){
             if (rcd) {
               util.getPricesInCity(rcd.city_id, rcd.name).then(function (value) {
                 res.send(value);
-              })
+              }).catch(function (reason) { console.log('===========reason1=========',reason); })
             }
             else {
               db.collection('cities').findOne(
-                {name: {$regex :new RegExp(names[1])}}
-              ).then(function (stateRcd) {
+                {name: {$regex :new RegExp(names[0])}}
+              ).then(function (stateRcd, err) {
                 if(stateRcd){
-                  util.getPricesInCity(rcd.city_id, rcd.name).then(function (value) {
+                  util.getPricesInCity(stateRcd.city_id, stateRcd.name).then(function (value) {
                     res.send(value);
-                  })
+                  }).catch(function (reason) { console.log('===========reason3=========',reason); })
                 }
                 else{
                   response['city'] = null;
                   res.send(JSON.stringify(response));
                 }
-              });
+              }).catch(function (reason) { console.log('===========reason2=========',reason); })
             }
           }).catch(function(err){
             console.log(err);
           });
-
-
         }
-      });
+      }).catch(function (reason) { console.log('===========reason5=========',reason); })
     });
   })
 });
+
+
+
 router.get('/search', function(req,res){
-  var url = 'mongodb://localhost:27017/fuel_api';
+  console.log('===========City=========',req.query.city);
+  var url = 'mongodb://pransh:12345@ds159274.mlab.com:59274/fuel_api';
+  // var url = 'mongodb://localhost:27017/fuel_api';
   mongodb.connect(url,function(mongoErr,db){
-    db.collection('cities').findOne({name : new RegExp(req.query.city, "i")}, function(rcd){
+    db.collection('cities').findOne({name : {$regex : new RegExp(req.query.city, "i")}}, function(err, rcd){
       var response = {};
       if (rcd) {
-        request({
-          url: "http://www.mypetrolprice.com/Default.aspx?LocationID=" + rcd.city_id}, function (errPrice, responsePrice, htmlPrice) {
-          var $ = cheerio.load(htmlPrice);
-          $('.hideInMobileDiv').filter(function () {
-            var data = $(this);
-            var cityName = rcd.name.split(',');
-            response['city'] = cityName[0];
-            if(!data.attr('id')) {
-              var text = data.text();
-              text = text.replace('Rs','');
-              response[data.siblings().first().text()] = "₹ " + text;
-              text='';
-            }
-          });
-          res.send(JSON.stringify(response));
-        });
-      }
-      else {
-        util.geocodeToName(req.query.city).then(function (value) {
-        }).catch(function (reason) {
-          console.log(reason);
+        util.getPricesInCity(rcd.city_id, rcd.name).then(function(response){
+          res.send(response);
         })
       }
-
+      else {
+        console.log('City %s not in DB, Searching and inserting city...', req.query.city);
+        util.insertCitiesIntoDb([req.query.city, ''], db).then(function(data){
+          console.log('Data Inserted  : '+data);
+          db.collection('cities').findOne({name:new RegExp(req.query.city)}).then(function(rcd, err){
+            var response = {};
+            if (rcd) {
+              // request({
+              //   url: "http://www.mypetrolprice.com/Default.aspx?LocationID=" + rcd.city_id, headers: {
+              //     'User-Agent': 'request'
+              //   }
+              // }, function (errPrice, responsePrice, htmlPrice) {
+              //   var $ = cheerio.load(htmlPrice);
+              //   $('.hideInMobileDiv').filter(function () {
+              //     var data = $(this);
+              //     response['city'] = names[0];
+              //     if(!data.attr('id')) {
+              //       var text = data.text();
+              //       text = text.replace('Rs','');
+              //       response[data.siblings().first().text()] = "₹ " + text;
+              //       text='';
+              //     }
+              //   });
+              //   res.send(JSON.stringify(response));
+              // });
+              util.getPricesInCity(rcd.city_id, rcd.name).then(function(data){
+                res.send(data);
+              })
+            }
+            else {
+              db.collection('cities').findOne(
+                {name: {$regex :new RegExp(names[1])}}
+              ).then(function (err,stateRcd) {
+                if(stateRcd){
+                  util.getPricesInCity(rcd.city_id, rcd.name).then(function (value) {
+                    res.send(value);
+                  }).catch(function (reason) { console.log('===========reason4=========',reason); })
+                }
+                else{
+                  response['city'] = null;
+                  res.send(JSON.stringify(response));
+                }
+              }).catch(function (error) {
+                console.log('===========rerror=========',error);
+              })
+            }
+          }).catch(function(err){
+            console.log(err);
+          });
+        }).catch(function(err){
+          console.log(err)
+        })
+      }
     });
-
-
-
   });
-
 });
 module.exports = router;
--- scrape/scrapeUtils.js
+++ scrape/scrapeUtils.js
@@ -4,14 +4,11 @@
 var request = require('request');
 var promise = require('promise');
 var cheerio = require('cheerio');
+var _ = require('lodash');
 
 module.exports = {
-
   geocodeToName : function(location, type){
-
     return new promise(function(resolve, reject){
-
-
       var stateName = '', cityName = '';
       if(type === 'gps') {
         var locationUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + location + '&key=AIzaSyD8CAcnPdwxtI4K4HsVbrbsch_l_zJNHFQ';
@@ -52,35 +49,37 @@ module.exports = {
         });
       }
       else if(type === 'city'){
-        var locationUrl = 'https://maps.googleapis.com/maps/api/geocode/json?city=' + location + '&key=AIzaSyD8CAcnPdwxtI4K4HsVbrbsch_l_zJNHFQ';
+        var locationUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + location + '&key=AIzaSyD8CAcnPdwxtI4K4HsVbrbsch_l_zJNHFQ';
         request(locationUrl, function (err, result) {
-          var cityData = JSON.parse(result.body);
-          var name = cityData.results[0].formatted_address;
-          var names = name.split(',');
-          console.log(names[0] + "," + names[1] + ",India");
-          return resolve(names[0] + "," + names[1] + ",India");
+          if(!err) {
+            var cityData = JSON.parse(result.body);
+            var name = cityData.results[0].formatted_address;
+            var names = name.split(',');
+            console.log(names[0] + "," + names[1] + ",India");
+            return resolve(names[0] + "," + names[1] + ",India");
+          }
+          else{
+            return reject(err);
+          }
         });
       }
     })
   },
-  insertCitiesIntoDb : function(formattedStateName,db){
+  insertCitiesIntoDb : function(name,db){
     return new promise(function(resolve,reject){
-
-      var priceUrl = 'http://www.mypetrolprice.com/?state='+formattedStateName;
+      var priceUrl = 'https://www.mypetrolprice.com/SearchResults.aspx?TextString='+name[0];
       request(priceUrl, function(err,response,html) {
-        console.log("Enter");
         var records = [];
         var $ = cheerio.load(html);
-        $('#ddlCity').filter(function () {
+        $('.searchDiv').filter(function () {
           var data = $(this);
+          var names = data.text().split(',');
           var record = {};
-          for (var i = 0; i < data.children().length; i++) {
-            record.name = data.children().get(i).children[0].data;
-            record.stateName = formattedStateName;
-            record.city_id = data.children().get(i).attribs.value;
-            records.push(record);
-            record = {};
-          }
+          record.name = names[0];
+          record.stateName = names[1];
+          record.city_id = data.attr('id');
+
+          records.push(record);
           db.collection('cities').insertMany(records);
           return resolve('success');
         });
@@ -90,24 +89,29 @@ module.exports = {
   getPricesInCity : function(cityId, name){
     return new promise(function(resolve,reject){
       var response = {};
+      var fuelNames = [];
+      var fuelPrices = [];
       request({
-        url: "http://www.mypetrolprice.com/Default.aspx?LocationID=" + cityId, headers: {
+        url: "http://www.mypetrolprice.com/" + cityId + "/Fuel-prices-in", headers: {
           'User-Agent': 'request'
         }
       }, function (errPrice, responsePrice, htmlPrice) {
         var $ = cheerio.load(htmlPrice);
-        $('.hideInMobileDiv').filter(function () {
-          var data = $(this);
+        $('.UCFuelName').filter(function () {
+          var fuelNameData = $(this);
           response['city'] = name.split(',')[0];
-          if(!data.attr('id')) {
-            var text = data.text();
-            text = text.replace('Rs','');
-            response[data.siblings().first().text()] = "₹ " + text;
-            text='';
-          }
+          fuelNames.push(fuelNameData.text());
         });
+        $('.UCPrice').filter(function(){
+          var fuelPriceData = $(this);
+          fuelPrices.push(fuelPriceData.text());
+        });
+        for(var i=0;i< fuelNames.length;i++) {
+          response[fuelNames[i]] = fuelPrices[i];
+        }
         return resolve(JSON.stringify(response));
       });
-    })
+
+    });
   }
 };
